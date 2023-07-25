368d78ad2bb6e0201dd453a0a07debbd99490e4f
Paloma Wink <paloma.wink@hotmail.com>
2023-07-24 22:29:28 -0300

    Arrumando formatação

--- src/routes/talker.router.js
+++ src/routes/talker.router.js
@@ -5,13 +5,13 @@ const { join } = require('path');
 const router = express.Router();
 
 router.get('/talker', async (_req, res) => {
-    const path = join(__dirname, '..', 'talker.json');
-    const talkers = await fs.readFile(path, 'utf8');
-    const data = JSON.parse(talkers);
-    if (!data) {
-      return res.status(200).json([]);
-    }
-    return res.status(200).json(data);
+  const path = join(__dirname, '..', 'talker.json');
+  const talkers = await fs.readFile(path, 'utf8');
+  const data = JSON.parse(talkers);
+  if (!data) {
+    return res.status(200).json([]);
+  }
+  return res.status(200).json(data);
 });
 
 router.get('/talker/:id', async (req, res) => {
