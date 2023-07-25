1c0b378841992d7eb3b6110e15584608dfbe4e36
Paloma Wink <paloma.wink@hotmail.com>
2023-07-24 22:01:26 -0300

    Requisito 1 concluido

--- src/routes/talker.router.js
+++ src/routes/talker.router.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const fs = require('fs').promises;
+const { join } = require('path');
 
 const router = express.Router();
 
-router.get('/poppy', (req, res) => {
-    res.send('OK');
+router.get('/talker', async (_req, res) => {
+    const path = join(__dirname, '..', 'talker.json');
+    const talkers = await fs.readFile(path, 'utf8');
+    const data = JSON.parse(talkers);
+    if (!data) {
+      return res.status(200).json([]);
+    }
+    return res.status(200).json(data);
 });
 
 module.exports = router;
