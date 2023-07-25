76a231c40289ddbac4a7e3d0e2dec0abaae13f0e
Paloma Wink <paloma.wink@hotmail.com>
2023-07-24 22:27:00 -0300

    Requisito 2 concluido

--- src/routes/talker.router.js
+++ src/routes/talker.router.js
@@ -14,4 +14,16 @@ router.get('/talker', async (_req, res) => {
     return res.status(200).json(data);
 });
 
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const path = join(__dirname, '..', 'talker.json');
+  const talkers = await fs.readFile(path, 'utf8');
+  const data = JSON.parse(talkers);
+  const talker = data.find((talk) => talk.id === parseInt(id, 10));
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(talker);
+});
+
 module.exports = router;
